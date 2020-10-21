e690e95fdb8a6b7d3a8d6b00dc774dd12fc35fb3
zi10ge <zi10ge@gmail.com>
2020-10-21 10:49:31 +0300

    test

--- App42PaaS-Java-MySQL-Sample/src/com/shephertz/app42/paas/sample/util/Util.java
+++ App42PaaS-Java-MySQL-Sample/src/com/shephertz/app42/paas/sample/util/Util.java
@@ -12,7 +12,7 @@ public class Util {
 	static File dbFile = null;
 	static {
 		try {
-			dbFile = new File("ROOT/Config.properties");
+			dbFile = new File("Config.properties");
 			dbProps.load(new FileInputStream(dbFile.getAbsolutePath()));
 		} catch (FileNotFoundException e) {
 			throw new RuntimeException(e);
